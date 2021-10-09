69a03d270792a64ebf97ed6a1c5d4731eae2b6af
Benno Schulenberg <bensberg@telfort.nl>
2021-10-09 12:55:41 +0200

    help: do not show ^S when --preserve is in effect
    This fixes https://savannah.gnu.org/bugs/?61317.
    
    Bug existed since version 5.0, since the Execute menu was overhauled.

--- src/global.c
+++ src/global.c
@@ -1214,7 +1214,8 @@ void shortcut_init(void)
 	add_to_sclist(MMAIN, "^J", '\n', do_justify_void, 0);
 #endif
 #ifdef ENABLE_SPELLER
-	add_to_sclist(MEXECUTE, "^S", 0, do_spell, 0);
+	if (!ISSET(PRESERVE))
+		add_to_sclist(MEXECUTE, "^S", 0, do_spell, 0);
 	add_to_sclist(MEXECUTE, "^T", 0, do_spell, 0);
 #endif
 #ifdef ENABLE_COLOR
